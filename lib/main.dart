7f888d234a9fe39356802588fb5f430f78f6e9ab
IamVoit <65310028+byIamVoit@users.noreply.github.com>
2023-10-24 19:51:07 +0200

    setup project
    Just as it says, setting up the project

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'homepage';
+
+void main() {
+  runApp(const MayApp());
+}
+
+class MayApp extends StatelessWidget {
+  const MayApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Container();
+  }
+}
